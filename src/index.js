fb495e8dde027e7f56bee8502b72a19d7040a822
Zack <107967808+zalsmith1996@users.noreply.github.com>
2024-05-17 07:00:57 -0400

    add .js pages, update json and config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+import renderHome from './home.js';
+import renderMenu from './menu.js';
+import renderAbout from './about.js';
+
+function initialize() {
+    const contentContainer = document.querySelector('#content');
+
+}
+
+initialize();
